ed037121f862d63589a03cefa7b25807f0bfed8f
Tolic598 <83219647+Tolic598@users.noreply.github.com>
2023-02-17 18:00:32 +0200

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,18 +1,9 @@
-function previewFile() {
-    var preview = document.querySelector('img');
-    var file    = document.querySelector('input[type=file]').files[0];
-    var reader  = new FileReader();
-  
-    reader.onloadend = function () {
-      preview.src = reader.result;
-    }
-  
-    if (file) {
-      reader.readAsDataURL(file);
-    } else {
-      preview.src = "";
-    }
-}
+let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
 
-let inputin = document.querySelector('.input_name');
+tg.expand(); //расширяем на все окно  
 
+tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
+tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
