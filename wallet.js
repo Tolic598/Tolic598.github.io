f8d9744e0af32335cbfc01248a7b0c7415b23069
Tolic598 <83219647+Tolic598@users.noreply.github.com>
2023-03-07 19:45:15 +0200

    Delete wallet.js

--- wallet.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("BTC").innerHTML = 0;
-document.getElementById("ETH").innerHTML = 0;
-document.getElementById("USDT").innerHTML = 0;
-document.getElementById("UAH").innerHTML = 0;
-document.getElementById("XRP").innerHTML = 0;
-document.getElementById("MINA").innerHTML = 0;
-document.getElementById("ASTR").innerHTML = 0;
-document.getElementById("APT").innerHTML = 0;
