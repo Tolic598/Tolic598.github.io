4d08b2ff18b67a1005afab036f79da7a08e0a23d
Tolic598 <83219647+Tolic598@users.noreply.github.com>
2023-03-07 19:46:35 +0200

    Add files via upload

---
+++ handers/__init__.py
@@ -0,0 +1,1 @@
+from handers import client
---
+++ wallet.js
@@ -0,0 +1,21 @@
+document.getElementById("BTC").innerHTML = 0;
+document.getElementById("ETH").innerHTML = 0;
+document.getElementById("USDT").innerHTML = 0;
+document.getElementById("UAH").innerHTML = 0;
+document.getElementById("XRP").innerHTML = 0;
+document.getElementById("MINA").innerHTML = 0;
+document.getElementById("ASTR").innerHTML = 0;
+document.getElementById("APT").innerHTML = 0;
+document.getElementById("BSW").innerHTML = 0;
+document.getElementById("HFT").innerHTML = 0;
+document.getElementById("CAKE").innerHTML = 0;
+document.getElementById("FLOW").innerHTML = 0;
+document.getElementById("LDO").innerHTML = 0;
+document.getElementById("HOOK").innerHTML = 0;
+document.getElementById("TWT").innerHTML = 0;
+document.getElementById("DASH").innerHTML = 0;
+document.getElementById("NEAR").innerHTML = 0;
+document.getElementById("DYDX").innerHTML = 0;
+document.getElementById("INCH").innerHTML = 0;
+document.getElementById("OP").innerHTML = 0;
+
