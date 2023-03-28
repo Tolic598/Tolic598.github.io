ced4ef7e99aa1e82005b89dbe79dc889d7803842
Tolic598 <83219647+Tolic598@users.noreply.github.com>
2023-03-28 21:12:00 +0300

    Delete keyboards directory

--- keyboards/kb.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.types.web_app_info import WebAppInfo
-'''#''''''''''''''''''''''''''''''''''''''''Обменик''''''''''''''''''''''''''''''''''''''''''''#'''
-b  = InlineKeyboardButton(text="Сайт", web_app=WebAppInfo(url="https://Tolic598.github.io"))
-kb = InlineKeyboardMarkup(row_width=1)
-kb.row(b)
