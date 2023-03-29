61d618427f10d5a7ed15a75f210c3089dbe6253c
Tolic598 <83219647+Tolic598@users.noreply.github.com>
2023-03-29 12:21:22 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -99,3 +99,23 @@ $( function() {
 $( function() {
   $("#drag_8").draggable();
 } );
+
+function Swap() {
+
+var width = window.innerWidth;
+
+if (width <= 599 && swapped === false) {
+
+swapDesign(“Mobile”);
+
+swapped = true;
+
+} else if (width > 599 && swapped === true) {
+
+swapDesign(“Desktop”);
+
+swapped = false;
+
+}
+
+}
