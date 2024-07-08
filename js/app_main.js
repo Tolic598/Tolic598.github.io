84e8e6b45d121b216151784a0d01a835d5c6979c
Tolic598 <83219647+Tolic598@users.noreply.github.com>
2024-07-08 16:39:13 +0300

    Update app_main.js

---
+++ js/app_main.js
@@ -0,0 +1,11 @@
+<script src="https://telegram.org/js/telegram-web-app.js"></script>
+    Telegram.WebApp.ready();
+
+    const initData = Telegram.WebApp.initData;
+    const initDataUnsafe = Telegram.WebApp.initDataUnsafe;
+
+    console.log(initData);
+    console.log(initDataUnsafe);
+
+    // Дополнительный код для взаимодействия с Telegram Web App
+
