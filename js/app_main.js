2e76cd924139e1a75585de7a7df44a5b1fe3f374
Tolic598 <83219647+Tolic598@users.noreply.github.com>
2024-07-08 22:33:36 +0300

    Add files via upload

---
+++ js/app_main.js
@@ -0,0 +1,7 @@
+Telegram.WebApp.ready();
+
+const initData = Telegram.WebApp.initData;
+const initDataUnsafe = Telegram.WebApp.initDataUnsafe;
+
+console.log(initData);
+console.log(initDataUnsafe);
